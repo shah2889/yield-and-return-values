49c948cbf9495f92d5b70f06225dde00e34f7f5d
iamShah <formal>
2020-10-09 10:12:24 -0400

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,30 @@
-require 'pry'
-
-def hello(array)
+def my_each(array)
   i = 0
   while i < array.length
-    yield(array[i])
-    i += 1
+     yield array[i]
+     i+=1
   end
+  array
 end
 
+def my_collect(array)
+  i = 0
+  new = []
+  while i < array.length
+     new << yield(array[i])
+     i += 1
+  end
+  new
+end
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+def my_select(collection)
+  i = 0
+  new = []
+  while i < collection.length
+     if yield(collection[i]) == true
+        new << collection[i]
+     end
+     i += 1
+  end
+  new
+end
